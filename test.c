b691398660b7e3632f03633d69da82ea640aa627
lcnihao <318082789@qq.com>
2019-04-21 10:49:00 +0800

    update

---
+++ test.c
@@ -0,0 +1,16 @@
+/*************************************************************************
+	> File Name: test.c
+	> Author: lichun
+	> Mail: 318082789@qq.com
+	> Created Time: 2019年04月18日 星期四 18时41分30秒
+ ************************************************************************/
+
+#include <stdio.h>
+#include <inttypes.h>
+
+int main() {
+    int64_t a;
+    scanf("%"PRId64, &a);
+    printf("%"PRId64, a);
+    return 0;
+}
