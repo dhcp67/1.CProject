9f03ad9968d9cf033f7147c9190942c0e825c217
lcnihao <318082789@qq.com>
2019-06-02 10:43:45 +0800

    add tcp_client.c

---
+++ chat/common.h
@@ -0,0 +1,24 @@
+/*************************************************************************
+	> File Name: common.h
+	> Author: lcnh 
+	> Mail: 318082789@qq.com
+	> Created Time: Sun Jun  2 10:32:52 2019
+ ************************************************************************/
+
+#ifndef DEBUG
+#define DBG(a, b) printf(a,b);
+#else 
+#define
+#endif
+
+#include <stdio.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pwd.h>
+#include <wait.h>
+#include <signal.h>
+
