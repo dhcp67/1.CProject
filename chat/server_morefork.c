6026ab77eafade9d3b5411f63bf231b77b60c08d
lcnihao <318082789@qq.com>
2019-06-02 18:54:18 +0800

    update

--- chat/server_morefork.c
+++ chat/server_morefork.c
@@ -21,6 +21,72 @@ void SignalFun() {
     wait(&status);
 }
 
+typedef struct Node{
+    int fd;
+    char name[20];
+    struct Node *next;
+} Node, *Linkedlist;
+
+Linkedlist linkedlist;
+
+int chac(Linkedlist linkedlist, char *name) { //查重
+    Node *p;
+    p = linkedlist;
+    int ret = 1;
+    while (p->next != NULL) {
+        if(strcmp(p->next->name, name) == 0) {
+            ret = 0;
+            printf("重复\n不能登录\n");
+        }
+        p = p->next;
+    }
+    return ret;
+}
+
+void insert(Linkedlist linkedlist, Node *node) {
+    Node *p;
+    p = linkedlist;
+    if(!chac(linkedlist, node->name)) {
+        return ;
+    }
+    while (p->next != NULL) {
+        p = p->next;
+    }
+    p->next = node;
+    return ;
+}
+
+
+Linkedlist CreateNode(int fd, char *name) {
+    Node *p = (Node *)malloc(sizeof(Node));
+    p->next = NULL;
+    p->fd = fd;
+    strcpy(p->name, name);
+    return p;
+}
+
+
+void delete(Linkedlist linkedlist, const char *name) {
+    Node *p, * q;
+    p  = linkedlist;
+    while (p->next != NULL && strcmp(p->next->name, name)) {
+        p = p->next;
+    }
+    if (p->next == NULL) return ;
+    q = p->next;
+    p->next = q->next;
+    free(q);
+}
+
+void output(Linkedlist linkedlist) {
+    Node *p;
+    p = linkedlist;
+    while (p->next != NULL) {
+        p = p->next;
+        printf("%s\t", p->name);
+    }
+    return ;
+}
 
 int main() {
     int port = 8731;
